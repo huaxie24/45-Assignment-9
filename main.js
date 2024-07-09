cc44aec6dded572184ac17558891dc0a6c2ef8f4
Laila Ali Laghari <162666511+huaxie24@users.noreply.github.com>
2024-07-09 05:50:53 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,2 @@
+var favouriteNumber = 24;
+console.log("My favourite number is ".concat(favouriteNumber));
---
+++ main.ts
@@ -0,0 +1,3 @@
+let favouriteNumber = 24;
+console.log(`My favourite number is ${favouriteNumber}`);
+
